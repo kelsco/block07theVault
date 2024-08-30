6e1ade6ff9e09771e6560796ec0024687225d396
Kelsco <kka245@gmail.com>
2024-08-29 20:37:32 -0400

    some message

---
+++ script.js
@@ -0,0 +1,112 @@
+// this is an alert
+alert(1+1)
+
+/*
+    this is a console
+    log statement
+*/
+console.log("this is a log mess", 1+1);
+
+
+// psudocode
+
+// alert the sum of 1 + 1
+alert(1+1);
+
+// log a message to the console
+console.log("this is a log message");
+
+//write a function that returns the largest prime
+
+
+// alert a message saying hi
+alert("hi");
+
+//add 1 + 1
+let firstsum = 1 + 1;
+
+console.log(firstsum, typeof firstsum)
+
+firstsum = "2";
+
+console.log(firstsum, typeof firstsum)
+
+//multiply by 2
+let firstProduct = firstsum * 2;
+
+//add 7
+
+// subtract 10
+
+//multiply by -1
+
+//whats the number
+
+//object
+let obj = {
+    name: "Kelsey",
+    age: 33,
+}
+
+console.log(obj.name);
+
+function x () {}
+console.log(undefined === undefined)
+
+let isRaining = false;
+let isSunny = true;
+
+
+// symbols
+// let sym = new Symbol(); 
+
+//Arrays
+let arr = [
+    "Kelsey",
+    33,
+    "dogs",
+    false
+];
+
+console.log('Array: ', arr[0], arr[2]);
+
+const animals = [
+    "wolf"
+];
+
+animals.push("cat")
+
+animals[2] = "dog";
+
+console.log(animals);
+
+const cat = {
+    name: "fluffykins",
+    color: "white",
+    friendly: true
+}
+
+cat.age = 2;
+
+delete cat.name;
+
+console.log ('cat: ', cat);
+
+
+const flavor = "orange";
+
+console.log(`My favorite flavor is: ${flavor}`);
+
+
+
+//comparison operators
+
+/*
+    = <-- assignment
+    == <-- equality (value)
+    === <-- identity (value + type)
+*/
+
+console.log("equality:", false == 7);
+
+
---
+++ theVault.js
@@ -0,0 +1,18 @@
+/* pseudocode
+Need to create 3 variables for step 1
+Each of those variables needs to perform one math oeration 
+that equals 1 of the 3 numbers
+that needs to then be displayed to the user
+*/
+
+let firstNumber = 5 * 2;
+
+let secondNumber = 80 / 2;
+
+let thirdNumber = 30 + 10 - 1
+
+let displayString = `You have received this message because 
+you have been chosen to open an important vault. 
+Here is the secret combination: ${firstNumber} - ${secondNumber} - ${thirdNumber}`
+
+alert (displayString)
